1ddbe09d3d0ec193f3abfb726834d5b5c48f6b1a
LuciusofLegend <luciusiber@gmail.com>
2025-01-25 13:07:23 -0500

    Rewrite RoundManager patch

--- src/QuotaOverhaul/Patches/RoundManager.cs
+++ src/QuotaOverhaul/Patches/RoundManager.cs
@@ -1,4 +1,5 @@
 using HarmonyLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Unity.Netcode;
@@ -9,122 +10,125 @@ namespace QuotaOverhaul
     public class SaveLootPatch
     {
         [HarmonyPrefix]
-        public static bool SkipOriginalDespawnProps()
+        public static void SkipOriginalDespawnProps()
         {
-            return false;
+            return;
         }
 
         [HarmonyPostfix]
-        public static bool CustomDespawnProps(bool despawnAllItems = false)
+        public static void CustomDespawnProps(bool despawnAllItems = false)
         {
-            GrabbableObject[] gObjects = UnityEngine.Object.FindObjectsOfType<GrabbableObject>();
-            RoundManager rManager = RoundManager.Instance;
+            GrabbableObject[] items = UnityEngine.Object.FindObjectsOfType<GrabbableObject>();
+
             if (despawnAllItems)
             {
-                return false;
-            }
-            System.Random RNG = new System.Random(StartOfRound.Instance.randomMapSeed + 369);
-            List<GrabbableObject> gObjectsAll = gObjects.ToList();
-            List<GrabbableObject> gObjectsInside = new List<GrabbableObject>();
-            foreach (GrabbableObject gObject in gObjects)
-            {
-                if (!(gObject.isInShipRoom || gObject.isHeld) || gObject.deactivated)
+                foreach (GrabbableObject item in items)
                 {
-                    Plugin.Log.LogInfo($"{gObject.name} Lost Outside");
-                    DespawnItem(gObject);
-                }
-                else
-                {
-                    gObjectsInside.Add(gObject);
+                    DespawnItem(item);
+                    Plugin.Log.LogInfo("Despawned all props");
                 }
             }
 
-            var result = gObjectsInside.ToLookup((GrabbableObject go) => go.itemProperties.isScrap);
-            List<GrabbableObject> gObjectsScrap = result[true].ToList();
-            List<GrabbableObject> gObjectsEquipment = result[false].ToList();
-
-            if (StartOfRound.Instance.allPlayersDead)
+            Random RNG = new Random(StartOfRound.Instance.randomMapSeed + 369);
+            List<GrabbableObject> itemsInside = new List<GrabbableObject>();
+            foreach (GrabbableObject item in items)
             {
-                if (RNG.NextDouble() >= (1f - (Config.saveAllChance?.Value ?? 0.25f)))
+                if (!(item.isInShipRoom || item.isHeld) || item.deactivated)
                 {
-                    Plugin.Log.LogInfo("All Saved");
+                    Plugin.Log.LogInfo($"{item.name} Lost Outside");
+                    DespawnItem(item);
                 }
                 else
                 {
-                    gObjectsScrap.RemoveAll((GrabbableObject go) => !go.IsSpawned);
-                    if (Config.valueSaveEnabled?.Value ?? false)
+                    itemsInside.Add(item);
+                }
+            }
+
+            if (!StartOfRound.Instance.allPlayersDead)
+            {
+                return;
+            }
+            if (RNG.NextDouble() >= (1f - (Config.saveAllChance?.Value ?? 0.25f)))
+            {
+                Plugin.Log.LogInfo("All Saved");
+                return;
+            }
+
+            var result = itemsInside.ToLookup((GrabbableObject go) => go.itemProperties.isScrap);
+            List<GrabbableObject> itemsScrap = result[true].ToList();
+            List<GrabbableObject> itemsEquipment = result[false].ToList();
+
+            itemsScrap.RemoveAll((GrabbableObject item) => !item.IsSpawned);
+            if (Config.valueSaveEnabled?.Value ?? false)
+            {
+                itemsScrap = itemsScrap.OrderByDescending((GrabbableObject item) => item.scrapValue).ToList();
+                int totalScrap = itemsScrap.Sum((GrabbableObject item) => item.scrapValue);
+                float saveScrap = totalScrap * (Config.valueSavePercent?.Value ?? 0.25f);
+                foreach (GrabbableObject item in itemsScrap)
+                {
+                    totalScrap -= item.scrapValue;
+                    Plugin.Log.LogInfo($"{item.name} Lost Value {item.scrapValue}");
+                    DespawnItem(item);
+                    if (totalScrap < saveScrap)
                     {
-                        gObjectsScrap = gObjectsScrap.OrderByDescending((GrabbableObject go) => go.scrapValue).ToList();
-                        int totalScrap = gObjectsScrap.Sum((GrabbableObject go) => go.scrapValue);
-                        float saveScrap = totalScrap * (Config.valueSavePercent?.Value ?? 0.25f);
-                        foreach (GrabbableObject gObject in gObjectsScrap)
-                        {
-                            totalScrap -= gObject.scrapValue;
-                            Plugin.Log.LogInfo($"{gObject.name} Lost Value {gObject.scrapValue}");
-                            DespawnItem(gObject);
-                            if (totalScrap < saveScrap)
-                            {
-                                Plugin.Log.LogInfo($"{totalScrap} Scrap Value Saved");
-                                break;
-                            }
-                        }
+                        Plugin.Log.LogInfo($"{totalScrap} Scrap Value Saved");
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                int lostSCount = 0;
+                foreach (GrabbableObject item in itemsScrap)
+                {
+                    if (RNG.NextDouble() >= (1f - (Config.saveEachChance?.Value ?? 0.5f)))
+                    {
+                        Plugin.Log.LogInfo($"{item.name} Saved");
                     }
                     else
                     {
-                        int lostSCount = 0;
-                        foreach (GrabbableObject gObject in gObjectsScrap)
+                        Plugin.Log.LogInfo($"{item.name} Lost");
+                        DespawnItem(item);
+                        lostSCount++;
+                        if (lostSCount >= (Config.scrapLossMax?.Value ?? int.MaxValue))
                         {
-                            if (RNG.NextDouble() >= (1f - (Config.saveEachChance?.Value ?? 0.5f)))
-                            {
-                                Plugin.Log.LogInfo($"{gObject.name} Saved");
-                            }
-                            else
-                            {
-                                Plugin.Log.LogInfo($"{gObject.name} Lost");
-                                DespawnItem(gObject);
-                                lostSCount++;
-                                if (lostSCount >= (Config.scrapLossMax?.Value ?? int.MaxValue))
-                                {
-                                    Plugin.Log.LogInfo($"Lost total {lostSCount}");
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                    if (Config.equipmentLossEnabled?.Value ?? false)
-                    {
-                        gObjectsEquipment.RemoveAll((GrabbableObject go) => !go.IsSpawned);
-                        int lostECount = 0;
-                        foreach (GrabbableObject gObject in gObjectsEquipment)
-                        {
-                            if (RNG.NextDouble() >= (1f - (Config.equipmentLossChance?.Value ?? 0.1f)))
-                            {
-                                Plugin.Log.LogInfo($"{gObject.name} Equipment Lost");
-                                DespawnItem(gObject);
-                                lostECount++;
-                                if (lostECount >= (Config.equipmentLossMax?.Value ?? int.MaxValue))
-                                {
-                                    Plugin.Log.LogInfo($"Equipment Lost total {lostECount}");
-                                    break;
-                                }
-                            }
+                            Plugin.Log.LogInfo($"Lost total {lostSCount}");
+                            break;
                         }
                     }
                 }
             }
-            return true;
-
-            void DespawnItem(GrabbableObject gObject)
+            if (Config.equipmentLossEnabled?.Value ?? false)
             {
-                if (gObject.isHeld && gObject.playerHeldBy != null)
+                itemsEquipment.RemoveAll((GrabbableObject go) => !go.IsSpawned);
+                int lostECount = 0;
+                foreach (GrabbableObject item in itemsEquipment)
                 {
-                    gObject.playerHeldBy.DropAllHeldItems();
+                    if (lostECount >= (Config.equipmentLossMax?.Value ?? int.MaxValue))
+                    { 
+                        break;
+                    }
+                    if (RNG.NextDouble() >= (1f - (Config.equipmentLossChance?.Value ?? 0.1f)))
+                    {
+                        Plugin.Log.LogInfo($"{item.name} Equipment Lost");
+                        DespawnItem(item);
+                        lostECount++;
+                    }
                 }
-                gObject.gameObject.GetComponent<NetworkObject>().Despawn(true);
-                if (rManager.spawnedSyncedObjects.Contains(gObject.gameObject))
-                {
-                    rManager.spawnedSyncedObjects.Remove(gObject.gameObject);
-                }
+                Plugin.Log.LogInfo($"Equipment Lost total {lostECount}");
+            }
+        }
+
+        public static void DespawnItem(GrabbableObject item)
+        {
+            if (item.isHeld && item.playerHeldBy != null)
+            {
+                item.playerHeldBy.DropAllHeldItems();
+            }
+            item.gameObject.GetComponent<NetworkObject>().Despawn(true);
+            if (RoundManager.Instance.spawnedSyncedObjects.Contains(item.gameObject))
+            {
+                RoundManager.Instance.spawnedSyncedObjects.Remove(item.gameObject);
             }
         }
     }
