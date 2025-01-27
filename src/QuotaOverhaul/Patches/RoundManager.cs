07d1c8b2cf2e6deeeb51688127a7d8ef93021c5a
LuciusofLegend <luciusiber@gmail.com>
2025-01-27 12:37:48 -0500

    Confirmed that the issue is with loot saving stuff

--- src/QuotaOverhaul/Patches/RoundManager.cs
+++ src/QuotaOverhaul/Patches/RoundManager.cs
@@ -1,4 +1,4 @@
-using HarmonyLib;
+/*using HarmonyLib;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -132,4 +132,4 @@ namespace QuotaOverhaul
             }
         }
     }
-}
+}*/
