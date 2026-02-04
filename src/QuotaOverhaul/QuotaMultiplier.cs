d6475f846ad936c0212e560bd1b92739bdec2f54
LuciusofLegend <luciusiber@gmail.com>
2026-02-04 17:51:28 -0500

    Use a better formatting method for this one debug log

--- src/QuotaOverhaul/QuotaMultiplier.cs
+++ src/QuotaOverhaul/QuotaMultiplier.cs
@@ -99,7 +99,7 @@ namespace QuotaOverhaul
             {
                 double beforeMultiplier = result;
                 result *= multiplier.Get();
-                Plugin.Log.LogDebug("Applying quota multiplier: " + multiplier.name + "\n" + beforeMultiplier + " * " + multiplier.Get() + " = " + result);
+                Plugin.Log.LogDebug($"Applying quota multiplier: {multiplier.name} \n {beforeMultiplier} + {multiplier.Get()} = {result}");
             }
             return (int)result;
         }
